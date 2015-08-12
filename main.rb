836a6c64f5d22e4a0d3d5653e95470a5a971ca83
jamesyu <zetaentranceserious@gmail.com>
2015-08-12 08:40:57 +0800

    Set up initial game

--- main.rb
+++ main.rb
@@ -5,5 +5,40 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'djivbskderopfjk234jio2jsk'
 
+get '/' do
+  if session[:player_name]
+    redirect '/game'  
+  else
+    redirect '/new_player'
+  end
+end
+
+get '/new_player' do
+  erb :new_player
+end
+
+post '/new_player' do
+  session[:player_name] = params[:player_name]
+  redirect '/game'
+end
+
+get '/game' do
+  # create a deck and put it in session
+  suits = %w[H D C S]
+  values = %w[2 3 4 5 6 7 8 9 10 A J Q K]
+  session[:deck] = suits.product(values).shuffle!
+
+  # deal cards
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+
+  erb :game
+end
+
+
 
 
