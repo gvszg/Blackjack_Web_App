7a8cafd291ab1a424a516844c6f36df51f1bd48e
jamesyu <zetaentranceserious@gmail.com>
2015-08-12 18:21:45 +0800

    Finish dealer's turn

--- main.rb
+++ main.rb
@@ -99,7 +99,7 @@ post '/game/player/hit' do
   player_total = calculate_total(session[:player_cards])
 
   if player_total == 21
-    @success = "Congratulations! #{session[:player_name]} hit blackjack!"
+    @success = "Congratulations! #{session[:player_name]} hits blackjack!"
     @show_hit_or_stay_buttons = false
   elsif player_total > 21
     @error = "Sorry, it looks like #{session[:player_name]} busted!"
@@ -112,8 +112,53 @@ end
 post '/game/player/stay' do
   @success = "#{session[:player_name]} has choosen to stay."
   @show_hit_or_stay_buttons = false
+
+  # It's dealer's turn
+  redirect '/game/dealer' 
+end
+
+get '/game/dealer' do
+  @show_hit_or_stay_buttons = false
+
+  # decision tree
+  dealer_total = calculate_total(session[:dealer_cards])
+
+  if dealer_total == 21
+    @error = "Sorry, dealer hits blackjack!"
+  elsif dealer_total > 21
+    @success = "Congratulations, dealer busted. #{session[:player_name]} won!"
+  elsif dealer_total >= 17 
+    # dealer stays
+    redirect '/game/compare'
+  else  
+    # dealer hits
+    @show_dealer_hit_button = true
+  end
+
   erb :game
 end
+
+post '/game/dealer/hit' do
+  session[:dealer_cards] << session[:deck].pop
+  redirect '/game/dealer'
+end
+
+get '/game/compare' do
+  @show_dealer_hit_button = false
+  player_total = calculate_total(session[:player_cards])
+  dealer_total = calculate_total(session[:dealer_cards])
+
+  if player_total < dealer_total
+    @error = "Sorry, #{session[:player_name]} lost."
+  elsif player_total > dealer_total
+    @success = "Congratulations, #{session[:player_name]} won!"
+  else
+    @success = "It's a tie!"
+  end
+
+  erb :game  
+end
+
 
 
 
